548b509c2feef124d794b4562da1e455cab2a550
sebastianbonelli <160727230+sebastianbonelli@users.noreply.github.com>
2024-05-16 03:44:27 +0200

    Finalized 01_process_images.py
    Cleaned up some file, function and variable names, added features function calls to the main loop.

--- python files/01_process_images.py
+++ python files/01_process_images.py
@@ -1,4 +1,4 @@
-ract_featuresimport os
+import os
 from os.path import exists
 import pandas as pd
 import numpy as np
@@ -11,124 +11,7 @@ from skimage.filters import threshold_otsu
 from skimage.measure import label, regionprops
 import matplotlib.pyplot as plt
 from scipy.ndimage import binary_erosion
-import extract_features.py
-
-#####################################################
-# Feature score functions
-#####################################################
-
-# We created separate csv's for each of the implemented feature functions and pasted the relevant function code
-# below when building each csv file (e.g. the following is for asymmetry)
-
-# For converting from the extracted scores to csv, skip to the next section
-
-def crop_im(image, mask):
-    image[mask==0] = 0
-
-    non_black_rows = []
-    for i, row in enumerate(image):
-        if np.any(row[:, :3] != [0, 0, 0]): 
-            non_black_rows.append(i)
-    rows = min(non_black_rows), max(non_black_rows)
-
-    non_black_columns = []
-    for j in range(image.shape[1]):  
-        if np.any(image[:, j, :3] != [0, 0, 0]): 
-            non_black_columns.append(j)
-    cols = min(non_black_columns), max(non_black_columns)
-
-    image = image[rows[0]:rows[1], cols[0]:cols[1],:]
-    return(image)
-
-#Asymmetry
-def rotation_crop(image, mask):
-    angles = [0, 22.5, 45, 67.5, 90, 112.5]
-    scores = []
-    images = []
-
-    for a in angles:
-        newim = image.copy()
-        newma = mask.copy()
-        newim = rotate(image, a)
-        newma = rotate(mask, a)
-
-        res = crop_im(newim, newma)
-        images.append(res)
-        symm = getsym(res)
-        avgsym = (symm[0] + symm[1])/2
-        scores.append(avgsym)
-    return max(scores)
-
-def getsym(image):
-    h, w = image.shape[:2]
-    hm = h // 2
-    wm = w // 2
-
-    if h % 2 == 0:  # Height is even
-        upper = image[:hm, :]
-        lower = image[hm:, :]
-    else:  # Height is odd
-        upper = image[:hm+1, :]
-        lower = image[hm:, :]
-
-    lower = np.flipud(lower)
-
-    upper = (upper > 0).astype(np.uint8)
-    lower = (lower > 0).astype(np.uint8)
-
-    vintersection = np.logical_and(upper, lower)
-
-    voverlap_percentage = (np.sum(vintersection) / np.sum(upper)) * 100
-#######
-    if w % 2 == 0:  # Width is even
-        left = image[:, :wm]
-        right = image[:, wm:]
-    else:  # Width is odd
-        left = image[:, :wm+1]
-        right = image[:, wm:]
-        
-    left = np.fliplr(left)
-
-    plt.imshow(right)
-    plt.imshow(left)
-
-    right = (right > 0).astype(np.uint8)
-    left = (left > 0).astype(np.uint8)
-
-    hintersection = np.logical_and(right, left)
-
-    hoverlap_percentage = (np.sum(hintersection) / np.sum(right)) * 100
-    #print("Percentage of v-overlap:", voverlap_percentage, "Percentage of h-overlap:", hoverlap_percentage)
-    return (voverlap_percentage, hoverlap_percentage)
-
-
-def segmentImage(input_img_path, mask_img_path, threshold=131, blockSize=15):
-
-    img = cv2.imread(input_img_path)
-
-
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
-    binary_img = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                       cv2.THRESH_BINARY_INV, threshold, blockSize)
-
-
-    mask = cv2.imread(mask_img_path, cv2.IMREAD_GRAYSCALE)
-
-    segmented_img = cv2.bitwise_and(binary_img, binary_img, mask=mask)
-
-    return segmented_img
-
-def calculateSegmentationScore(segmented_img, mask):
-
-    area_mask = cv2.countNonZero(mask)
-    area_segmented_img = cv2.countNonZero(segmented_img)
-    score = (area_segmented_img / area_mask) * 100
-
-    return score
-
-
+import extract_features as feat
 
 #-------------------
 # Main script
@@ -139,9 +22,11 @@ def calculateSegmentationScore(segmented_img, mask):
 file_data = 'ISIC-2017_Training_Part3_GroundTruth.csv'
 path_image = 'images'
 path_mask = 'masks'
-  
+
+n_imgs = 10 #change to fit your data set size
+
 #Name of saved csv file
-file_features = 'compiled2.csv'
+file_features = 'compiled_features.csv'
 
 #Read meta-data
 df = pd.read_csv(file_data)
@@ -153,24 +38,20 @@ label = np.array(df['melanoma'])
 num_images = len(image_id)
 
 #Feature and other csv headings array:
-feature_names = ['Diagnosis','Asymmetry','Blue_gray_granules','Depigmentation','Compactness','Roundness','Mean_Laplacian','Std_Dev_Laplacian','Sat SD', 'Val SD', 'Hue SD','image ID']
+feature_names = ['Diagnosis','Asymmetry','Blue_gray_granules','Depigmentation','Val SD','image ID']
 num_features = len(feature_names)
-features = np.zeros([num_images,num_features], dtype=np.float16)  
+features = np.zeros([n_imgs,num_features], dtype=np.float16)
 
 #main loop
-for i in np.arange(0,2000):
+for i in np.arange(0,n_imgs):
     
     # Define filenames related to image
     file_image = path_image + os.sep + image_id[i] + ".jpg"
     maskid = image_id[i]
     maskid = maskid + "_segmentation.png"
     file_mask = path_mask + os.sep + maskid
-
-    print(file_image)
-    print(file_mask)
     
     if exists(file_image):
-        print("does exist")
         
         # Read the image
         im = plt.imread(file_image)
@@ -178,13 +59,17 @@ for i in np.arange(0,2000):
 
         mask = plt.imread(file_mask)
 
-        asymm = rotation_crop(im, mask)
-        compact = extract_features.get_compactness(mask)
-        round = extract_features.get_roundness(mask)
+        asymm = feat.rotation_crop(im, mask)
+        bgb = feat.calculateSegmentationScore(file_image, file_mask)
+        depig = feat.depigmentation(im, mask)
+        sd_val = feat.getColorFeatures(im, mask)
+
         # Storing variable in array
         features[i,1] = asymm
-        features[i,4] = compact
-        features[i,5] = round
+        features[i,2] = bgb
+        features[i,3] = depig
+        features[i,4] = sd_val
+
 
         if df.loc[i, "melanoma"]==1:
             features[i,0] = 1
